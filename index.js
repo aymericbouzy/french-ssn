5182629c84fe27707a7b81ef2d73e6c90d2f3cda
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2018-02-23 09:48:09 +0100

    more

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-export const parsor = ssn => {
+const between = (lower, value, upper) => lower <= value && value <= upper
+
+const padKey = key => (key < 10 ? `0${key}` : `${key}`)
+
+export const getControlKey = partialSsn => {
+  return padKey(97 - parseInt(partialSsn.replace(/[abAB]/, "0")) % 97)
+}
+
+export const parse = ssn => {
+  const result = {}
   if (typeof ssn !== "string") {
     throw new Error("French Social Security Number must be a string")
   }
   if (ssn.length !== 15) {
     throw new Error("French Social Security Number must be 15 characters long")
   }
-  if (!/^\d{6}[0-9aAbB]\d{8}$/.test(ssn)) {
+  if (!/^\d{6}[0-9abAB]\d{8}$/.test(ssn)) {
     throw new Error(
       "French Social Security Number only allows digits, except for the letters a and b in 7th position",
     )
   }
+  const re = /^(\d)(\d{2})(\d{2})(\d{5}|2[abAB]\d{3})(\d{3})(\d{2})$/
+  const parts = re.exec(ssn)
+  if (!parts) {
+    throw new Error()
+  }
+  const [_, gender, year, month, place, rank, controlKey] = parts
+  if (between(1, month, 12)) {
+    result.month = month
+  } else if (
+    between(30, month, 42) ||
+    between(50, month, 99) ||
+    month === "20"
+  ) {
+    result.month = "unkown"
+  } else {
+    throw new Error("Month appears to be incorrect")
+  }
+  switch (gender) {
+    case "1":
+    case "3":
+    case "7":
+      result.gender = "male"
+      break
+    case "2":
+    case "4":
+    case "8":
+      result.gender = "female"
+      break
+    default:
+      throw new Error("Gender has to be among 1, 2, 3, 4, 7 and 8")
+  }
+  const partialSsn = `${gender}${year}${month}${place}${rank}`
+  const expectedControlKey = getControlKey(partialSsn)
+  if (controlKey != expectedControlKey) {
+    throw new Error(
+      `Control key does not match (expected ${expectedControlKey})`,
+    )
+  }
+  return result
 }
--- index.test.js
+++ index.test.js
@@ -1,44 +1,84 @@
-import { parsor } from "./index"
+import { parse, getControlKey } from "./index"
+
+const tap = arg => {
+  console.log(arg)
+  return arg
+}
+
+const makeSSN = ({
+  gender = "2",
+  year = "89",
+  month = "04",
+  place = "78342",
+  rank = "163",
+  controlKey,
+}) => {
+  const partial = `${gender}${year}${month}${place}${rank}`
+  return `${partial}${controlKey || tap(getControlKey(partial))}`
+}
 
 describe("invalid SSN", () => {
   it("is a string", () => {
-    expect(() => parsor()).toThrow(
+    expect(() => parse()).toThrow(
       "French Social Security Number must be a string",
     )
-    expect(() => parsor(123)).toThrow(
+    expect(() => parse(123)).toThrow(
       "French Social Security Number must be a string",
     )
-    expect(() => parsor({})).toThrow(
+    expect(() => parse({})).toThrow(
       "French Social Security Number must be a string",
     )
-    expect(() => parsor([1, 23, 34])).toThrow(
+    expect(() => parse([1, 23, 34])).toThrow(
       "French Social Security Number must be a string",
     )
   })
 
   it("has 15 characters", () => {
-    expect(() => parsor("12345671234567")).toThrow(
+    expect(() => parse("12345671234567")).toThrow(
       "French Social Security Number must be 15 characters long",
     )
-    expect(() => parsor("1234567812345678")).toThrow(
+    expect(() => parse("1234567812345678")).toThrow(
       "French Social Security Number must be 15 characters long",
     )
   })
 
   it("has only (mostly) digits", () => {
-    expect(() => parsor("12345123451234a")).toThrow(
+    expect(() => parse(makeSSN({ year: "5a" }))).toThrow(
       "French Social Security Number only allows digits, except for the letters a and b in 7th position",
+    )
+  })
+
+  it("has possible month of birth", () => {
+    expect(() => parse(makeSSN({ month: "45" }))).toThrow(
+      "Month appears to be incorrect",
+    )
+  })
+
+  it("has possible gender", () => {
+    expect(() => parse(makeSSN({ gender: "0" }))).toThrow(
+      "Gender has to be among 1, 2, 3, 4, 7 and 8",
     )
   })
 })
 
 describe("correct SSN", () => {
-  it("has a correct check")
+  it("has a correct control key", () => {
+    expect(() => parse(makeSSN({ controlKey: "23" }))).toThrow(
+      "Control key does not match (expected 49)",
+    )
+  })
 })
 
 describe("result", () => {
+  it("provides the gender", () => {
+    ;[1, 3, 7].map(gender => {
+      expect(parse(makeSSN({ gender })).gender).toBe("male")
+    })
+    ;[(2, 4, 8)].map(gender => {
+      expect(parse(makeSSN({ gender })).gender).toBe("female")
+    })
+  })
   it("provides the title")
-  it("provides the gender")
   it("provides the year of birth")
   it("provides the month of birth")
   it("provides the date of birth (1 month approximate)")
@@ -57,7 +97,9 @@ describe("result", () => {
   })
 
   describe("corner cases", () => {
-    it("works for Corsica (both cases)")
+    it("works for Corsica (both upperCase and lowerCase)")
     it("works for Algeria before 1962")
+    it("works with unknown birth month")
+    it("works when controlKey is below 10")
   })
 })
